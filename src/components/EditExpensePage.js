02d17d3d6a6aac920c84911e1e7b10689848c7f0
MIraK1E <mike.pattana@gmail.com>
2018-03-11 01:27:22 +0700

    setup startRemoveExpense

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ExpenseForm from './ExpenseForm'
 import { connect } from 'react-redux'
-import { editExpense, removeExpense } from '../actions/expenses'
+import { editExpense, startRemoveExpense } from '../actions/expenses'
 
 class EditExpensePage extends React.Component {
     editExpense = (expense) => {
@@ -9,7 +9,7 @@ class EditExpensePage extends React.Component {
         this.props.history.push('/')
     }
     removeExpense = () => {
-        this.props.removeExpense({ id: this.props.expense.id })
+        this.props.startRemoveExpense({ id: this.props.expense.id })
         this.props.history.push('/')
     }
     render() {
@@ -55,7 +55,7 @@ const mapStateToProps = (state, props) => {
 const mapDispatchToProps = (dispatch, props) => {
    return {
        editExpense: (id, expense) => dispatch(editExpense(id, expense)),
-       removeExpense: (data) => dispatch(removeExpense(data))
+       startRemoveExpense: (data) => dispatch(startRemoveExpense(data))
    }
 }
 
